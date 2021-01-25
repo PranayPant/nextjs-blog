f8459d57590a8908499e44fdf0c028f28793b27a
PranayPant <ppantg1@gmail.com>
2021-01-25 14:58:15 -0600

    styling

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -10,23 +10,23 @@ export default function FirstPost(props) {
          </Head>
          <header>
             <div className={styles.header}>
-               <div className={styles.item}>
+               <div className={styles.title}>
                   <h1>Kanye Quotes</h1>
                </div>
-               <div className={styles.item}>
-                  <Link href="/">
-                     <a>Back Home</a>
-                  </Link>
+               <div className={styles.homeLink}>
+                  <div className={styles.logo}>
+                     <Link href="/">
+                        <img src='/icons/home.svg' />
+                     </Link>
+                  </div>
                </div>
             </div>
          </header>
-         <div className={styles.main}>
-            <main>
-               <div>
-                  <span>{props.quote}</span>
-               </div>
-            </main>
-         </div>
+         <main>
+            <div className={styles.main}>
+               <div className={styles.content}><span className={styles.quote}>{props.quote}</span></div>
+            </div>
+         </main>
       </div>
    );
 }
