5c27c86d741c88f1d90ceea37e1f5302eadef0d7
PranayPant <ppant@ucsd.edu>
2021-01-24 21:03:03 -0600

    Update first-post.js

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,34 +1,44 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from './styles.module.scss';
 
-import Head from 'next/head'
-import Link from 'next/link'
-import styles from './styles.module.scss'
-
-export default function FirstPost(props){
-    return(
-        <div>
-            <Head>
-                <title>Quote of the Ye</title>
-            </Head>
-            <h1>First Post</h1>
-            <Link href='/'><a>Back Home</a></Link>
-            <div className={styles.imgContainer}><img src='/images/selfie.jpg' /></div>
-            <div>
-                <span>Kanye said whaaat???</span>
-                <br />
-                <span>{props.quote}</span>
+export default function FirstPost(props) {
+   return (
+      <div className={styles.container}>
+         <Head>
+            <title>Quote of the Ye</title>
+         </Head>
+         <header>
+            <div className={styles.header}>
+               <div className={styles.item}>
+                  <h1>Kanye Quotes</h1>
+               </div>
+               <div className={styles.item}>
+                  <Link href="/">
+                     <a>Back Home</a>
+                  </Link>
+               </div>
             </div>
-        </div>
-    )
+         </header>
+         <div className={styles.main}>
+            <main>
+               <div>
+                  <span>{props.quote}</span>
+               </div>
+            </main>
+         </div>
+      </div>
+   );
 }
 
 export async function getStaticProps() {
-    // Get external data from the file system, API, DB, etc.
-    const response = await fetch('https://api.kanye.rest')
-    const data = await response.json()
-  
-    // The value of the `props` key will be
-    //  passed to the `Home` component
-    return {
-      props: data
-    }
-  }
+   // Get external data from the file system, API, DB, etc.
+   const response = await fetch('https://api.kanye.rest');
+   const data = await response.json();
+
+   // The value of the `props` key will be
+   //  passed to the `Home` component
+   return {
+      props: data,
+   };
+}
