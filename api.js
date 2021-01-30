42a5483e87bc0670f0bd5dc829a1b95de158e8b2
PranayPant <ppantg1@gmail.com>
2021-01-29 18:54:26 -0600

    added google sign in

--- api.js
+++ api.js
@@ -1,4 +1,6 @@
 import * as Realm from 'realm-web';
+import googleOneTap from 'google-one-tap';
+
 import {connect, MONGO_APP} from './db'
 
 export async function fetchQuote(){
@@ -39,3 +41,33 @@ export async function loginEmailPassword({email, password}){
         console.error("Failed to login user", err)
     }
 }
+
+export function loginGoogle(){
+    const credentials = Realm.Credentials.google(process.env.NEXT_PUBLIC_OAUTH_REDIRECT_URI);
+    // Calling logIn() opens a Google authentication screen in a new window.
+    MONGO_APP.logIn(credentials).then(user => {
+      // The logIn() promise will not resolve until you call `handleAuthRedirect()`
+      // from the new window after the user has successfully authenticated.
+      alert(`Logged in with id: ${user.id}`);
+    })
+    // When the user is redirected back to your app, handle the redirect to
+    // save the user's access token and close the redirect window. This
+    // returns focus to the original application window and automatically
+    // logs the user in.
+    Realm.handleAuthRedirect();
+}
+
+export function loginGoogleOneTap(){
+    console.log('ontap')
+    // Open the Google One Tap menu
+    googleOneTap({ client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID }, async (response) => {
+        console.log('ionside')
+        // Upon successful Google authentication, log in to Realm with the user's credential
+        const credentials = Realm.Credentials.google(response.credential)
+        const user = await app.logIn(credentials);
+        console.log(`Logged in with id: ${user.id}`);
+    });
+    console.log('outside')
+
+}
+
